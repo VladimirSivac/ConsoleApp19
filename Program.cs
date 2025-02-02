3242f83e4ee39c9b6334d2a4e66333ada3e67e31
VladimirSivac <morndes@gmail.com>
2025-02-02 21:37:48 +0200

    task-4 done

--- Program.cs
+++ Program.cs
@@ -10,7 +10,13 @@ namespace ConsoleApp19
     {
         static void Main(string[] args)
         {
-           
+            double sum = Sum(150, -200, 0.2);
+            Console.WriteLine($"Сумма первых 150 членов: {sum}");
+        }
+        
+        static double Sum(int n, double a1, double d)
+        {
+            return n / 2 * (2 * a1 + (n - 1) * d);
         }
     }
 }
