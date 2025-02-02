a7efc29f8121b547a7e2af310f4cf99d3db5ec34
VladimirSivac <morndes@gmail.com>
2025-02-02 21:28:28 +0200

    task-3 done

--- Program.cs
+++ Program.cs
@@ -10,7 +10,18 @@ namespace ConsoleApp19
     {
         static void Main(string[] args)
         {
-           
+            double total = TotalSavings(10);
+            Console.WriteLine($"Сумма через 10 месяцев: {total} руб.");
+        }
+        
+        static double TotalSavings(int months)
+        {
+            double sum = 0;
+            for (int i = 0; i < months; i++)
+            {
+                sum += 100 + i * 50;
+            }
+            return sum;
         }
     }
 }
